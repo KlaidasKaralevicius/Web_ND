c5947d0562fa2935aec8ba0784c13e4dfa3e9cbc
Kaemri <klaidas.karalevicius@gmail.com>
2025-04-09 13:48:26 +0300

    add password save

--- js/login.js
+++ js/login.js
@@ -1,9 +1,11 @@
 function saveLoginInfo() {
     const username = document.getElementById("username").value;
     const email = username + "@gmail.com";
+    const password = document.getElementById("password").value;
 
     localStorage.setItem("user_name", username);
     localStorage.setItem("user_email", email);
+    localStorage.setItem("password", password)
 
     window.location.href = "main.html";
 }
--- js/profile.js
+++ js/profile.js
@@ -1,5 +1,6 @@
 const username = localStorage.getItem("user_name") || "MusicIO";
 const email = localStorage.getItem("user_email") || "musicIO@gmail.com";
+const password = localStorage.getItem("password");
 
 // replace content
 document.getElementById("profile-username").textContent = username;
